835579f9816062e57b8f0ef72a6d49a43107d36f
avminon <avminon@gmail.com>
2016-01-15 18:32:05 +0800

    Fixed const ROLE_ADMIN and Activity model

--- app/User.php
+++ app/User.php
@@ -14,7 +14,7 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
 {
     use Authenticatable, CanResetPassword;
 
-    protected $adminRole = 'admin';
+    const ROLE_ADMIN = 'admin';
 
     protected $table = 'users';
 
@@ -24,27 +24,27 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
 
     public function lesson()
     {
-        return $this->hasMany(Lesson::class,'user_id');
+        return $this->hasMany(Lesson::class, 'user_id');
     }
 
     public function activity()
     {
-        return $this->hasMany(Activity::class,'user_id');
+        return $this->hasMany(Activity::class, 'user_id');
     }
 
     public function follower()
     {
-        return $this->hasMany(Follow::class,'follower_id');
+        return $this->hasMany(Follow::class, 'follower_id');
     }
 
     public function followee()
     {
-        return $this->hasMany(Follow::class,'followee_id');
+        return $this->hasMany(Follow::class, 'followee_id');
     }
 
     public function isAdmin()
     {
-        if($this->type == $adminRole)
+        if ($this->type == User::ROLE_ADMIN)
         {
             return true;
         }
